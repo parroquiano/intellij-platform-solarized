4eea40f53a79bbcb7563a267d4995e074b63d044
Alex <4lex49@zoho.com>
2018-11-26 01:41:13 +0300

    ~ Reformat script code

--- install.bat
+++ install.bat
@@ -1,5 +1,5 @@
 @echo off
-setlocal enabledelayedexpansion
+setlocal EnableDelayedExpansion
 
 set SCHEMES_TO_INSTALL="Solarized Dark.icls" "Solarized Light.icls"
 
@@ -8,35 +8,31 @@ set DETECTING_PATH=config\options\project.default.xml
 set COLORS_DIR=config\colors
 
 for %%d in (%SCHEMES_TO_INSTALL%) do (
-	set themeFile=%~dp0%%~d
-	if not exist "!themeFile!" (
-		echo Error: file %%d not found! >&2
-		pause
-		exit /b 1
-	)
+  set themeFile=%~dp0%%~d
+  if not exist "!themeFile!" (
+    echo Error: file %%d not found! >&2
+    pause & exit /b 1
+  )
 )
 
 for /f "tokens=*" %%a in (
-	'dir /b /ad "%IDE_SETTINGS_ROOT%" 2^>nul ^| findstr /i /r /C:"^\..*$"'
+    'dir /b /ad "%IDE_SETTINGS_ROOT%" 2^>nul ^| findstr /i /r /C:"^\..*$"'
 ) do (
-	if exist "%IDE_SETTINGS_ROOT%\%%a\%DETECTING_PATH%" (
-		set ideSettingsColorPath=%IDE_SETTINGS_ROOT%\%%a\%COLORS_DIR%
-		
-		if not exist "!ideSettingsColorPath!" (
-			mkdir "!ideSettingsColorPath!" >nul
-		)
-		
-		for %%d in (%SCHEMES_TO_INSTALL%) do (
-			set themeFile=%~dp0%%~d
-			copy /y "!themeFile!" "!ideSettingsColorPath!" >nul
-		)
-		
-		if !ERRORLEVEL! equ 0 (
-			set ideName=%%a
-			set ideName=!ideName:~1!
-			echo The schemes installed for "!ideName!" successfully.
-		)
-	)	
+  if exist "%IDE_SETTINGS_ROOT%\%%a\%DETECTING_PATH%" (
+    set dest=%IDE_SETTINGS_ROOT%\%%a\%COLORS_DIR%
+    if not exist "!dest!" (
+      mkdir "!dest!" >nul
+    )
+    for %%d in (%SCHEMES_TO_INSTALL%) do (
+      set themeFile=%~dp0%%~d
+      copy /y "!themeFile!" "!dest!" >nul
+    )
+    if !ERRORLEVEL! equ 0 (
+      set ideName=%%a
+      set ideName=!ideName:~1!
+      echo The schemes were successfully installed for "!ideName!".
+    )
+  )
 )
 
 :exit
--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
-#! /bin/sh
+#!/bin/bash
 
 SCHEMES_TO_INSTALL=("Solarized Dark.icls" "Solarized Light.icls")
 
@@ -6,36 +6,27 @@ IDE_SETTINGS_ROOT=$HOME
 DETECTING_PATH=config/options/project.default.xml
 COLORS_DIR=config/colors
 
-for ((i=0; i < ${#SCHEMES_TO_INSTALL[@]}; i++))
-do
-    theme=$(dirname $0)/${SCHEMES_TO_INSTALL[$i]}
-    if [ ! -f "$theme" ]
-    then
-        echo Error: file \"$theme\" not found! >&2
-        exit 1
-    else
-        SCHEMES_TO_INSTALL[$i]=$theme
-    fi
+for ((i=0; i < ${#SCHEMES_TO_INSTALL[@]}; i++)); do
+  theme=$(dirname $0)/${SCHEMES_TO_INSTALL[$i]}
+  if [ ! -f "$theme" ]; then
+    echo Error: file \"$theme\" not found! >&2
+    exit 1
+  else
+    SCHEMES_TO_INSTALL[$i]=$theme
+  fi
 done
 
-for ide in `ls -A "$IDE_SETTINGS_ROOT"`
-do
-    if [[ $ide =~ ^\..*$ ]]
-    then
-        if [ -f "$IDE_SETTINGS_ROOT/$ide/$DETECTING_PATH" ]
-        then
-            dest=$IDE_SETTINGS_ROOT/$ide/$COLORS_DIR
-            if [ ! -d "$dest" ]
-            then
-                mkdir "$dest"
-            fi
-        
-            cp "${SCHEMES_TO_INSTALL[@]}" "$dest"
-            
-            if [ "$?" = "0" ]
-            then
-                echo The schemes installed for \"${ide:1}\" successfully.
-            fi
-        fi
+for ide in `ls -A "$IDE_SETTINGS_ROOT"`; do
+  if [[ $ide =~ ^\..*$ ]]; then
+    if [ -f "$IDE_SETTINGS_ROOT/$ide/$DETECTING_PATH" ]; then
+      dest=$IDE_SETTINGS_ROOT/$ide/$COLORS_DIR
+      if [ ! -d "$dest" ]; then
+        mkdir "$dest"
+      fi
+      cp "${SCHEMES_TO_INSTALL[@]}" "$dest"
+      if [ "$?" = "0" ]; then
+        echo The schemes were successfully installed for \"${ide:1}\".
+      fi
     fi
+  fi
 done
